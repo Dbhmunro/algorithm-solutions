b1041dfa22d6df71166a1901988793adf7bb84bf
dbhmunro <dbhmunro@gmail.com>
2020-12-16 21:20:33 -0500

    remove duplicates from sorted array solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Two sum problem
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-// Using the two-pointer approach
+// Using the two-pointer approach - O(n)
 
 var twoSum = function(numbers, target) {
     let left = 0
@@ -23,7 +23,7 @@ var twoSum = function(numbers, target) {
 
 // Sorted Squares problem
 // https://leetcode.com/problems/squares-of-a-sorted-array/
-// Using two(three) pointer approach
+// Using two(three) pointer approach - O(n)
 
 var sortedSquares = function(nums) {
     let leftPointer = 0
@@ -44,13 +44,47 @@ var sortedSquares = function(nums) {
         }
         resultsPointer--
         
-//         condensed lines of code example
+    //         condensed lines of code example
 
-//         let largerSquare = Math.max(leftPointerSquare, rightPointerSquare)
-//         results[resultsPointer] = largerSquare
-//         resultsPointer--
-//         largerSquare === leftPointerSquare ? leftPointer++ : rightPointer--
+    //         let largerSquare = Math.max(leftPointerSquare, rightPointerSquare)
+    //         results[resultsPointer] = largerSquare
+    //         resultsPointer--
+    //         largerSquare === leftPointerSquare ? leftPointer++ : rightPointer--
     }
     
     return results
+};
+
+
+// Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Using two pointer approach - O(n)
+// Original splice solution is O(n^2)
+
+var removeDuplicates = function(nums) {
+    //     let first = 0
+    //     let second = 1
+        
+    //     while (second < nums.length) {
+    //         if (nums[first] === nums[second]) {
+    //             nums.splice(first, 1)
+    //         } else {
+    //             first++
+    //             second++
+    //         }
+    //     }
+        
+    //     return nums.length
+        
+    let i = 0
+    
+    for (let j = 0; j < nums.length; j++) {
+        
+        if (nums[i] != nums[j]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    
+    return i + 1
 };
